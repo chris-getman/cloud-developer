1dd6f57f7fa63a36c4c4e91a56998a1741df14ea
= <cg.getman@gmail.com>
2021-05-09 08:20:52 -0400

    adding in the endpoint and ionic updates to gethings working

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,20 +16,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  app.get("/filteredimage", async (req, res) => {
+    if (!req.query.image_url) {
+      res.status(422);
+      res.send("You need to provide an image_url");
+    }
+    else {
+      var filteredUrl: string;
+      await filterImageFromURL(req.query.image_url).then( path => {
+        filteredUrl = path;
+      }
+        );
+      res.sendFile(filteredUrl);
+      res.on('finish',  () => {
+        deleteLocalFiles([filteredUrl]);
+      })
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
